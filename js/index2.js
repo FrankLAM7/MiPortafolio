43e4f750391efc9dbe3db662ffdd65093328e8d1
FrankLAM7 <51126368+FrankLAM7@users.noreply.github.com>
2020-01-29 10:51:55 -0500

    version 1.0.0
    asd

---
+++ js/index2.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $("div.bhoechie-tab-menu>ul.list-group>a").click(function(e) {
+      e.preventDefault();
+      $(this).siblings('a.active').removeClass("active");
+      $(this).addClass("active");
+      var index = $(this).index();
+      $("div.bhoechie-tab>div.bhoechie-tab-content").removeClass("active");
+      $("div.bhoechie-tab>div.bhoechie-tab-content").eq(index).addClass("active");
+  });
+});
+$(function() {
+    // Sidebar toggle behavior
+    $('#sidebarCollapse').on('click', function() {
+      $('#sidebar, #content').toggleClass('active');
+    });
+});
---
+++ js/main.js
@@ -0,0 +1,36 @@
+window.onload = function(){
+    var contenido = document.getElementById("contentpage");
+    contenido.setAttribute("src","https://franklam7.github.io/VelocidadSegunHuellaDeFrenado/");
+
+
+    document.getElementById("mapas").addEventListener("click",function(){
+        contenido.setAttribute("src","https://franklam7.github.io/mapas-codigo-5/#");
+
+    });
+    document.getElementById("huellafrenado").addEventListener("click",function(){
+        contenido.setAttribute("src","https://franklam7.github.io/VelocidadSegunHuellaDeFrenado/");
+    });
+    // https://franklam7.github.io/ProyectoJS/index.html#
+    document.getElementById("blog").addEventListener("click",function(){
+        contenido.setAttribute("src","https://franklam7.github.io/ProyectoJS/index.html#");
+    });
+    //https://franklam7.github.io/PeliculasApp/index.html#/
+    document.getElementById("peliculas").addEventListener("click",function(){
+        contenido.setAttribute("src","https://franklam7.github.io/PeliculasApp/index.html#/");
+    });
+    //https://franklam7.github.io/CajeroAutomaticoJS-Grupo3/CajeroAutomatico/index.html
+    document.getElementById("cajero").addEventListener("click",function(){
+        contenido.setAttribute("src","https://franklam7.github.io/CajeroAutomaticoJS-Grupo3/CajeroAutomatico/index.html");
+    });
+    //https://franklam7.github.io/ProyectoRutas/ProyectoRutas/index.html
+    document.getElementById("rutas").addEventListener("click",function(){
+        contenido.setAttribute("src","https://franklam7.github.io/ProyectoRutas/ProyectoRutas/index.html");
+    });
+
+$(function() {
+    // Sidebar toggle behavior
+    $('#sidebarCollapse').on('click', function() {
+      $('#sidebar, #content').toggleClass('active');
+    });
+  });
+}
